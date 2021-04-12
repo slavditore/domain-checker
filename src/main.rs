21f0bad685b59940ae10e0fa10c449170ef98427
slavditore <slavditore@protonmail.com>
2021-04-12 21:35:55 +0300

    Initial reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::fs;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = Path::new("./src/config.yml");
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+    println!("{}", contents)
 }
