06c5b08503d1f9baf03a1776e372f98f7007ba36
slavditore <slavditore@protonmail.com>
2021-04-12 23:26:23 +0300

    Initial reading of file

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+)
 
 func main() {
+	file := "./config/config.yml"
 	fmt.Println("This is a reload of the project")
+	fmt.Println("Trying to read file")
+	config, _ := ioutil.ReadFile(file)
+	fmt.Println(string(config))
 }
