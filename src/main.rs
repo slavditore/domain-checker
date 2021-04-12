e934b07254c0cc6af62563af19c33fd6f8c96341
Slavditore <slavditore@protonmail.com>
2021-04-12 20:01:56 +0000

    Feature/add config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+mod file;
+
+
 fn main() {
-    println!("Hello, world!");
+    let file: String  = "./src/config.yml".to_string();
+    let content = file::read_file(&file);
+    println!("{}", content);
+    let domains = file::get_domains(content);
+    for d in domains{
+        println!("You'll check the next domain: {}", d)
+    }
 }
