40e97f659d5bdede9a571ae32a3f6280c2435f51
slavditore <slavditore@protonmail.com>
2021-04-12 22:59:08 +0300

    Configured a getting of domains for check

--- src/file.rs
+++ src/file.rs
@@ -7,14 +7,16 @@ pub fn read_file(file: &String) -> String {
     let filename = Path::new(&file);
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
-    return contents
+    contents
 }
 
-pub fn get_domains(content: String){
+pub fn get_domains(content: String) -> Vec<String>{
+    let mut domains = Vec::<String>::new();
     let doc = &(YamlLoader::load_from_str(&content).unwrap())[0];
     for g in doc["groups"].as_vec().unwrap(){
         for d in g["domains"].as_vec().unwrap(){
-            println!("{}", d.as_str().unwrap())
+            domains.push(d.as_str().unwrap().to_string());
         }
     }
+    domains
 }
--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,8 @@ fn main() {
     let file: String  = "./src/config.yml".to_string();
     let content = file::read_file(&file);
     println!("{}", content);
-    file::get_domains(content)
+    let domains = file::get_domains(content);
+    for d in domains{
+        println!("You'll check the next domain: {}", d)
+    }
 }
