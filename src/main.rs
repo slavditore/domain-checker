f10bcd57eed9d2c8387f8361c1f886e1683c2c0b
slavditore <slavditore@protonmail.com>
2021-04-12 22:27:39 +0300

    Test implementation of yaml processor

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 mod file;
+extern crate yaml_rust;
+use yaml_rust::{YamlLoader};
+
 
 fn main() {
     let file: String  = "./src/config.yml".to_string();
     let content = file::read_file(&file);
-    println!("{}", content)
+    println!("{}", content);
+    let doc = &(YamlLoader::load_from_str(&content).unwrap())[0];
+    for g in doc["groups"].as_vec().unwrap(){
+        for d in g["domains"].as_vec().unwrap(){
+            println!("{}", d.as_str().unwrap())
+        }
+    }
 }
