ed2a83bf8dead80bb0ff92d5c2670eba94b48ba0
Slavditore <slavditore@protonmail.com>
2021-06-08 00:14:14 +0300

    Add initial flags support

--- main.go
+++ main.go
@@ -3,16 +3,21 @@ package main
 import (
 	"log"
 
+	flag "github.com/spf13/pflag"
 	certcheck "gitlab.com/slavditore/check-domain/v2/internal/cert_check"
 	configv1 "gitlab.com/slavditore/check-domain/v2/internal/config.v1"
 	output_plain "gitlab.com/slavditore/check-domain/v2/internal/output_log"
 )
 
+var (
+	argFileName = flag.String("config", "./config/config.yml", "path to file with domains")
+)
+
 func main() {
-	file := "./config/config.yml"
+	flag.Parse()
 	log.Println("This is a reload of the project")
 	log.Println("Trying to read file")
-	domains := configv1.ReadConfig(&file)
+	domains := configv1.ReadConfig(argFileName)
 	log.Println("Process the logs")
 	domains = certcheck.CheckDomains(domains)
 	output_plain.PlainOutput(&domains)
